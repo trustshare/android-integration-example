116524852ca2ac29b846757b3b9d809534df995e
Simon Holmes <srsholmes@gmail.com>
2023-04-04 08:50:41 +0000

    Update example to use API product (#1)
    * chore: android example update
    
    * button
    
    * chore: finally get the view displaying the JSON response of the completed intent
    
    * chore: use prod instead of staging
    
    * chore: tidy up

--- README.md
+++ README.md
@@ -1,58 +1,56 @@
 # android-integration-example
-An example showing how to integrate trustshare using android.
+
+An example showing how to integrate trustshare using Android.
 
 ## How it works
-This example shows how an android application can be integrated with the trustshare web sdk.
+This example shows how an Android application can be integrated with the trustshare web sdk.
 
-The app uses a `WebView` to load a page specifically developed for mobile integration. 
+The app uses a `WebView` to load a page specifically developed for mobile integration.
 
-Please refer to the [web sdk documentation](https://docs.trustshare.co/sdk/web-sdk) for further information and definitions for the various actions that can be carried out.
+Please refer to the [web sdk documentation](https://docs.trustshare.io/guides/sdks/web-sdk) for further information and definitions for the various actions that can be carried out.
+
+### Getting started
+
+Clone the repo and cd into the server directory.
+
+Create a `.env` file and add the following:
+
+```bash
+TRUSTSHARE_PRIVATE_API_KEY=your-private-key
+TRUSTSHARE_PUBLIC_API_KEY=your-public-key
+```
+Then run the following commands:
+```bash
+yarn # Install dependencies
+yarn start
+```
+
+Open up the project in Android Studio and click build. It will open up an emulator with the example app running.
+
+The example app
+When the app launches, it will generate a create a payment intent by requesting a client_secret from the example server. The client secret is then used to load the webview with the url https://checkout.trustshare.io.
 
 ### The WebView
-The webview loads a route at `https://${YOUR_SUBDOMAIN}.trustshare.co/mobile-sdk`.
+The webview loads a route at https://checkout.trustshare.io, with your generated client secret on the end as a query parameter.
 
-The url requires a query parameter of at least `type` and `handlerName`.
+The resulting url should look similar to this:
 
-The `type` parameter tells us what sort of `Action` you would like to do and the `handlerName` tells us where to post the state updates to.
+https://checkout.trustshare.io/process?s=CLIENT_SECRET
 
-The resulting url should look similar to this: 
-
-`https://demo.trustshare.co/mobile-sdk?type=checkout&handlerName=myHandlerName`.    
-
-### Actions
-
-The `mobile-sdk` route can handle a variety of [Actions](/android-integration-example/app/src/main/java/com/example/android_integration_example/Definitions.kt#L9-L35) which can be implemented. The enum of `Actions` is below. 
-
-```
-Checkout
-Topup
-Dispute
-Return
-Release
-```
-
-Each action has its own required parameters defined in their respective data classes. See [Definitions](/android-integration-example/app/src/main/java/com/example/android_integration_example/Definitions.kt#L9-L35) for types.
 ### State updates
-The example android app receives state updates from the webview using the [JSBridge](/android-integration-example/app/src/main/java/com/example/android_integration_example/MainActivity#L161-L171) class. 
-This class requires a function called postMessage which is used by trustshare to send messages back to the android app. The messages passed will be JSON which is documented in the [web sdk documentation](https://docs.trustshare.co/sdk/web-sdk). 
+The example app receives state updates from the webview using the WebViewClient class.
+If the message name is the same as the provided handler, which defaults to "trustshareHandler", the message is passed on to the provided callback.
 
-### Query strings
-The webview should use a query string to communicate to the webview which [Action](/android-integration-example/app/src/main/java/com/example/android_integration_example/Definitions.kt#L67) is intended to carry out. 
+From here, we can use a Gson library to deserialize the json messages and pass them into objects.
 
-### Custom user agent
-The webview needs to set a custom user agent containing the string `"trustshare-sdk/android"`, otherwise the page will not load.
+### Closing the webview
+When the webview is closed, a handler will be called to let the Android app know. If the checkout has been successful, there will be a project_id and a checkout_id in the message.
 
-This can be done with the following code: 
+### Help and support
+Please feel free to reach out to us on slack or contact support if you need further guidance on integration with Android, and we'll do our best to help.
 
-```kotlin
-    webView?.settings?.userAgentString =
-      webView?.settings?.userAgentString + " trustshare-sdk/android/1.0"
-```
-
-## Help and support
-
-Please feel free to reach out to us on slack or [contact support](mailto:support@trustshare.co) if you need further guidance on integration with iOS, and we'll do our best to help.
-
-## Improvements
+### Improvements
 We are always looking for feedback, so we can provide the best possible developer experience.
-If you have any comments, suggestions, questions or feature requests, please [let us know](mailto:engineers@trustshare.co).
+If you have any comments, suggestions, questions or feature requests, please let us know.
+
+
--- app/src/main/java/com/example/android_integration_example/MainActivity.kt
+++ app/src/main/java/com/example/android_integration_example/MainActivity.kt
@@ -4,175 +4,188 @@ import android.content.Context
 import android.net.Uri
 import android.net.http.SslError
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.Message
 import android.util.Log
+import android.view.View
 import android.view.WindowManager
 import android.webkit.*
 import android.webkit.WebView.WebViewTransport
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.LifecycleOwner
+import com.example.trustshare_android_integration.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
+import java.net.URL
 
 
-class MainActivity : AppCompatActivity() {
-  private var webView: WebView? = null
-  private var webViewPopup: WebView? = null
-  private var mContext: Context? = null
-  private var builder: AlertDialog? = null
-  private val callback = fun(message: String) {
-    Log.d("trustshare.message", message)
-    return
-  }
+suspend fun fetchClientSecret(): String {
+    val client = OkHttpClient()
+    val request = Request.Builder().url("http://10.0.2.2:9987/createPaymentIntent").build()
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    // The arguments needed to integrate with the trustshare web sdk.
-    val webviewArgs = WebViewArgs(
-      checkoutAction,
-      "demo",
-      "trustshareAndroid",
-      callback
-    )
-
-    super.onCreate(savedInstanceState)
-    createPrimaryWebView(webviewArgs)
-    setContentView(webView)
-    mContext = this
-  }
-
-
-  private fun <T> createPrimaryWebView(args: WebViewArgs<T>) {
-    webView = WebView(this)
-    webView?.settings?.userAgentString =
-      webView?.settings?.userAgentString + " trustshare-sdk/android/1.0"
-    webView?.settings?.javaScriptEnabled = true
-    webView?.settings?.domStorageEnabled = true
-    webView!!.webViewClient = UriWebViewClient()
-    webView!!.webChromeClient = UriChromeClient()
-    webView?.settings?.setSupportMultipleWindows(true)
-    webView?.addJavascriptInterface(JSBridge(args.callback), args.handlerName)
-    val url = makeURL(args)
-    webView?.loadUrl(url)
-  }
-
-  private fun <T> makeURL(args: WebViewArgs<T>): String {
-    val builder = Uri.Builder()
-    val subdomain = args.subdomain
-    val action = args.action
-
-    builder.scheme("https")
-      .authority("${subdomain}.trustshare.co")
-      .appendPath("mobile-sdk")
-      .appendQueryParameter("subdomain", subdomain)
-      .appendQueryParameter("handlerName", args.handlerName)
-
-    if (action is CheckoutArgs) {
-      builder.appendQueryParameter("type", "checkout")
-      if (action.to != null) {
-        builder.appendQueryParameter("to", action.to)
-      }
-      if (action.currency != null) {
-        builder.appendQueryParameter("currency", action.currency.currencyVal)
-      }
-      if (action.amount != null) {
-        builder.appendQueryParameter("amount", action.amount)
-      }
-      if (action.description != null) {
-        builder.appendQueryParameter("description", action.description)
-      }
+    val response = withContext(Dispatchers.IO) {
+        client.newCall(request).execute()
     }
 
-    if (action is DisputeArgs) {
-      builder.appendQueryParameter("type", "dispute")
-      builder.appendQueryParameter("token", action.token)
+    if (response.isSuccessful) {
+        val json = JSONObject(response.body?.string())
+        val clientSecret = json.getString("client_secret")
+        return clientSecret
+    } else {
+        // Handle error
+        throw Exception("Error fetching client secret")
+    }
+}
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+    var clientSecret = "your_client_secret_will_be_here"
+    private var handlerName = "trustshareHandler"
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var mainLayout: ConstraintLayout
+    private var webView: WebView? = null
+    private var webViewPopup: WebView? = null
+    private var mContext: Context? = null
+    private var builder: AlertDialog? = null
+    private lateinit var jsonText: TextView
+    private var button: Button? = null
+
+    private val callback = fun(message: String) {
+        Log.d("trustshare.message", message)
+        val json = JSONObject(message)
+        val type = json.getString("type")
+        Log.d("trustshare.type", type)
+        if (type == "complete") {
+            mainLayout.post {
+                webView?.visibility = WebView.GONE
+            }
+            handler.post {
+                // Hide the create intent button
+                button?.visibility = View.GONE
+                // Destroy the web view
+                webView?.destroy()
+                webView = null
+                // Show the JSON in the text view
+                setContentView(R.layout.activity_main)
+                jsonText = findViewById(R.id.json_text)
+                jsonText.setText(message)
+            }
+        }
+        return
+    }
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mContext = this
+        setContentView(R.layout.activity_main)
+        mainLayout = findViewById(R.id.main_layout) // Add this line
+        jsonText = findViewById(R.id.json_text) // Add this line
+        button = findViewById<Button>(R.id.button)
+        button?.setOnClickListener {
+            coroutineScope.launch {
+                try {
+                    clientSecret = fetchClientSecret()
+                    val webviewArgs = WebViewArgs(handlerName, callback)
+                    createPrimaryWebView(webviewArgs)
+                    setContentView(webView)
+                } catch (e: Exception) {
+                    Log.e("error_fetching_secret", e.message.toString())
+                }
+            }
+        }
     }
 
-    if (action is TopupArgs) {
-      builder.appendQueryParameter("type", "topup")
-      builder.appendQueryParameter("token", action.token)
-      if (action.amount != null) {
-        builder.appendQueryParameter("amount", action.amount)
-      }
+    private fun createPrimaryWebView(args: WebViewArgs) {
+        val url = makeURL()
+        runOnUiThread {
+            webView = WebView(this)
+            webView?.settings?.userAgentString =
+                webView?.settings?.userAgentString + " trustshare-sdk/android/1.0"
+            webView?.settings?.javaScriptEnabled = true
+            webView?.settings?.domStorageEnabled = true
+            webView?.webViewClient = UriWebViewClient()
+            webView?.webChromeClient = UriChromeClient()
+            webView?.settings?.setSupportMultipleWindows(true)
+            webView?.addJavascriptInterface(JSBridge(args.callback), args.handlerName)
+            setContentView(webView)
+            webView?.loadUrl(url)
+        }
     }
 
-    if (action is ReturnArgs) {
-      builder.appendQueryParameter("type", "return")
-      builder.appendQueryParameter("token", action.token)
-      if (action.amount != null) {
-        builder.appendQueryParameter("amount", action.amount)
-      }
+    fun makeURL(): String {
+        val builder = Uri.Builder()
+        builder.scheme("https").authority("checkout.trustshare.io").path("/process")
+            .appendQueryParameter("s", clientSecret).appendQueryParameter("handler", handlerName)
+        val encodedQuery = builder.build().encodedQuery?.replace("+", "%2B")
+        val urlString = "https://checkout.trustshare.io/process?$encodedQuery"
+        return URL(urlString).toString()
     }
 
-    if (action is ReleaseArgs) {
-      builder.appendQueryParameter("type", "release")
-      builder.appendQueryParameter("token", action.token)
-      if (action.amount != null) {
-        builder.appendQueryParameter("amount", action.amount)
-      }
+    private class UriWebViewClient : WebViewClient() {
+        override fun onReceivedSslError(
+            view: WebView, handler: SslErrorHandler, error: SslError
+        ) {
+            Log.d("onReceivedSslError", "onReceivedSslError")
+        }
     }
 
-    val url = builder.build().toString()
-    return url
-  }
+    inner class UriChromeClient : WebChromeClient() {
+        override fun onCreateWindow(
+            view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message
+        ): Boolean {
+            webViewPopup = mContext?.let { WebView(it) }
+            webViewPopup?.isVerticalScrollBarEnabled = false
+            webViewPopup?.isHorizontalScrollBarEnabled = false
+            webViewPopup?.webViewClient = UriWebViewClient()
+            webViewPopup?.webChromeClient = UriChromeClient()
+            webViewPopup?.settings?.javaScriptEnabled = true
 
-  // This is needed to deal with the open banking flow popup flow.
-  // TODO: Check if only needed for local dev only. May be able to be removed.
-  private class UriWebViewClient : WebViewClient() {
-    override fun onReceivedSslError(
-      view: WebView, handler: SslErrorHandler,
-      error: SslError
-    ) {
-      Log.d("onReceivedSslError", "onReceivedSslError")
+            builder =
+                mContext?.let { AlertDialog.Builder(it, AlertDialog.BUTTON_POSITIVE).create() }
+            builder!!.setView(webViewPopup)
+            builder!!.show()
+            builder!!.window!!.clearFlags(
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM
+            )
+
+            val transport = resultMsg.obj as WebViewTransport
+            transport.webView = webViewPopup
+            resultMsg.sendToTarget()
+            return true
+        }
+
+        // When then window is closed, destroy the popup view and builder.
+        override fun onCloseWindow(window: WebView) {
+            webViewPopup?.destroy()
+            builder?.dismiss()
+        }
     }
-  }
+    
+    inner class JSBridge(val cb: (message: String) -> Unit) {
+        @JavascriptInterface
+        // This function must be called postMessage.
+        // This is the function trustshare will call from the JS interface.
+        // The call back passed in is the callback defined at the entry point for the web view.
+        fun postMessage(message: String) {
+            cb(message)
+            if (message == "trustshareCloseWebView") {
+                destroyWebView()
+            }
+        }
 
-  inner class UriChromeClient : WebChromeClient() {
-    override fun onCreateWindow(
-      view: WebView?, isDialog: Boolean,
-      isUserGesture: Boolean, resultMsg: Message
-    ): Boolean {
-      webViewPopup = mContext?.let { WebView(it) }
-      webViewPopup?.isVerticalScrollBarEnabled = false
-      webViewPopup?.isHorizontalScrollBarEnabled = false
-      webViewPopup?.webViewClient = UriWebViewClient()
-      webViewPopup?.webChromeClient = UriChromeClient()
-      webViewPopup?.settings?.javaScriptEnabled = true
-
-      builder =
-        AlertDialog.Builder(this@MainActivity, AlertDialog.BUTTON_POSITIVE).create()
-      builder!!.setView(webViewPopup)
-      builder!!.show()
-      builder!!.window!!.clearFlags(
-        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-          or WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM
-      )
-
-      val transport = resultMsg.obj as WebViewTransport
-      transport.webView = webViewPopup
-      resultMsg.sendToTarget()
-      return true
+        private fun destroyWebView() {
+            // Destroy web view and return back to app containing web view.
+            Log.d("trustshare.message", "Destroy function called.")
+        }
     }
-
-    // When then window is closed, destroy the popup view and builder.
-    override fun onCloseWindow(window: WebView) {
-      webViewPopup?.destroy()
-      builder?.dismiss()
-    }
-  }
-
-  inner class JSBridge(val cb: (message: String) -> Unit) {
-    @JavascriptInterface
-    // This function must be called postMessage.
-    // This is the function trustshare will call from the JS interface.
-    // The call back passed in is the callback defined at the entry point for the web view.
-    fun postMessage(message: String) {
-      cb(message)
-      if (message == "trustshareCloseWebView") {
-        destroyWebView()
-      }
-    }
-
-    private fun destroyWebView() {
-      // Destroy web view and return back to app containing web view.
-      Log.d("trustshare.message", "Destroy function called.")
-    }
-  }
 }
